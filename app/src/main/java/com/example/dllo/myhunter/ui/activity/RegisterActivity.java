9ffc533922030e42e724116758c6330d337132a1
杨柳 <1845549666@qq.com>
2016-08-12 15:28:29 +0800

    注册界面的布局

---
+++ app/src/main/java/com/example/dllo/myhunter/ui/activity/RegisterActivity.java
@@ -0,0 +1,23 @@
+package com.example.dllo.myhunter.ui.activity;
+
+import com.example.dllo.myhunter.R;
+
+/**
+ * Created by dllo on 16/8/12.
+ */
+public class RegisterActivity extends AbsBaseActivity {
+    @Override
+    protected int setLayout() {
+        return R.layout.activity_register;
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initDatas() {
+
+    }
+}
