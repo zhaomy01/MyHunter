5e901e3d3343211f63e1269d7b3157975c825bbc
仲丽伟 <1291799713@qq.com>
2016-08-12 15:24:49 +0800

    登录界面的布局

---
+++ app/src/main/java/com/example/dllo/myhunter/ui/activity/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.dllo.myhunter.ui.activity;
+
+import com.example.dllo.myhunter.R;
+
+/**
+ * Created by zlw on 16/8/12.
+ * 登录界面
+ */
+public class LoginActivity extends AbsBaseActivity {
+    @Override
+    protected int setLayout() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initDatas() {
+
+    }
+}
