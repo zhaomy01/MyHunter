11571d8a53f5cdaffd0035d2ca3cf4448f8753c5
杨柳 <1845549666@qq.com>
2016-08-16 20:17:53 +0800

    全部城市框架

---
+++ app/src/main/java/com/example/dllo/myhunter/ui/activity/AllCityActivity.java
@@ -0,0 +1,21 @@
+package com.example.dllo.myhunter.ui.activity;
+
+/**
+ * Created by dllo on 16/8/16.
+ */
+public class AllCityActivity extends AbsBaseActivity {
+    @Override
+    protected int setLayout() {
+        return 0;
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initDatas() {
+
+    }
+}
